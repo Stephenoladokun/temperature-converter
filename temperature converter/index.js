7bf64a032e91de4e86aa0c434b3ea27e1190b17e
Stephenoladokun <ytstephen111@gmail>
2024-10-21 11:41:08 +0100

    initial commit

--- temperature converter/index.js
+++ temperature converter/index.js
@@ -1,25 +1,42 @@
+const input = document.getElementById("input");
+const select1 = document.getElementById("select1");
+const select2 = document.getElementById("select2");
+const btn = document.getElementById("submit");
+const ansText = document.getElementById("degree");
 
-
-
-document.getElementById("submit").onclick = function(){
-    const inputtext = document.getElementById("input").value;
-    console.log(inputtext);
-    let select1 = document.getElementById("select1").value;
-    if(select1 == fah){
-        console.log("yes")
+btn.onclick = function (){
+    if(select1.value == 'Fahrenheit' && select2.value == 'Celsius'){
+        console.log("fah to cel is");
+    const fahval = input.value;
+    let ans = (fahval - 32) * 5/9;
+    const decimal = ans.toFixed(2);
+    ansText.textContent = `°${decimal}C`;
     }
-   
 
 
-
-
-    // if(select1 == op){
-        // console.log((inputtext - 32) * 5/9);
-    // }
+    if(select1.value == 'Celsius' && select2.value == 'Fahrenheit'){
+        console.log("cel to fah is");
+        const celval = input.value;
+        let ans = (9/5 * celval) + 32;
+        const decimal = ans.toFixed(2);
+        ansText.textContent = `${decimal}F`;
+    }
+    
+    
+    if(select1.value == 'Fahrenheit' && select2.value == 'Fahrenheit'){
+        console.log("cel to fah is");
+        const fahval = input.value;
+        let ans = (fahval);
+        ansText.textContent = `°${ans}C`;
+    }
+    
+    
+    if(select1.value == 'Celsius' && select2.value == 'Celsius'){
+        console.log("cel to fah is");
+        const celval = input.value;
+        let ans = (celval);
+        ansText.textContent = `${ans}F`;
+    }
+    
 
 }
-
-let fah = document.getElementById("fah");
- 
-
-
